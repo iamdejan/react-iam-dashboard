e50eb79a4340a266fa502db0ee7ca163e166ed11
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-11-04 16:24:24 +0700

    test: add Vitest with integration to Mantine

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -8,4 +9,9 @@ export default defineConfig({
     TanStackRouterVite(),
     react()
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.mjs",
+  },
 });
