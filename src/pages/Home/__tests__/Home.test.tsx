6a3e89c278c17e638389c58604cc6dcfccea96d2
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-11-04 17:13:14 +0700

    test: add unit test

--- src/pages/Home/__tests__/Home.test.tsx
+++ src/pages/Home/__tests__/Home.test.tsx
@@ -20,4 +20,29 @@ describe("Home", () => {
     expect(subTitle).toBeInstanceOf(HTMLHeadingElement);
     expect(subTitle.style.fontWeight).toBe("normal");
   });
+
+  it("should render paper items", () => {
+    const { container } = render(<Home />);
+
+    const papers = container.querySelectorAll("div.mantine-Paper-root");
+    expect(papers).toHaveLength(4);
+    for(let paper of papers) {
+      expect(paper).toBeInstanceOf(HTMLDivElement);
+      paper = paper as HTMLDivElement;
+
+      const icon = paper.querySelector("svg");
+      expect(icon).not.toBeUndefined();
+      expect(icon).not.toBeNull();
+
+      const paperTitle = paper.querySelector("h5");
+      expect(paperTitle).not.toBeUndefined();
+      expect(paperTitle?.textContent).not.toBeUndefined();
+      expect(paperTitle?.textContent).not.toBeNull();
+
+      const text = paper.querySelector("p");
+      expect(text).not.toBeUndefined();
+      expect(text?.textContent).not.toBeUndefined();
+      expect(text?.textContent).not.toBeNull();
+    }
+  });
 });
